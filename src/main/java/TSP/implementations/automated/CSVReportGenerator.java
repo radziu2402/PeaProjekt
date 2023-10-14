1ec6d866d02ee6573acd4d7479d9893a9d4ff279
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-10-14 22:31:58 +0200

    Correct CSV report to generate elapsed time not cost

--- src/main/java/TSP/implementations/automated/CSVReportGenerator.java
+++ src/main/java/TSP/implementations/automated/CSVReportGenerator.java
@@ -8,28 +8,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CSVReportGenerator implements ReportGenerator {
-    private final HashMap<Integer, Integer> resultsForCsv = new HashMap<>();
+    private final HashMap<Integer, Long> resultsForCsv = new HashMap<>();
     private final HashMap<Integer, Integer> numberOfResultsForCsv = new HashMap<>();
     private final ArrayList<MeasuredAlghorithmResult> results = new ArrayList<>();
 
     @Override
     public String generateReport() {
         StringBuilder report = new StringBuilder();
-        report.append("Liczba wierzchołków;Koszt\n");
+        report.append("Liczba wierzchołków;Średni czas\n");
         for (MeasuredAlghorithmResult result : results) {
             AlgorithmResult algorithmResult = result.getAlgorithmResult();
             int numberOfVertices = algorithmResult.getNumberOfVertices();
             if(resultsForCsv.containsKey(numberOfVertices)) {
-                resultsForCsv.replace(numberOfVertices, resultsForCsv.get(numberOfVertices) + algorithmResult.getMinCost());
+                resultsForCsv.replace(numberOfVertices, resultsForCsv.get(numberOfVertices) + result.getExecutionTime());
                 numberOfResultsForCsv.replace(numberOfVertices, numberOfResultsForCsv.get(numberOfVertices) + 1);
             } else {
-                resultsForCsv.put(numberOfVertices, algorithmResult.getMinCost());
+                resultsForCsv.put(numberOfVertices, result.getExecutionTime());
                 numberOfResultsForCsv.put(numberOfVertices, 1);
             }
         }
 
         for (int nOfVertices : resultsForCsv.keySet()) {
-            int result = resultsForCsv.get(nOfVertices)/numberOfResultsForCsv.get(nOfVertices);
+            long result = resultsForCsv.get(nOfVertices)/numberOfResultsForCsv.get(nOfVertices);
                 report.append(nOfVertices);
                 report.append(';');
                 report.append(result);
